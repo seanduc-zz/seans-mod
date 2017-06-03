b7cbbd46dc39074f036d8c1c88c6d8b1c6779646
Dru Jensen <drujensen@gmail.com>
2017-06-03 08:35:11 -0700

    initial checkin

---
+++ src/main/java/com/example/examplemod/SeansMod.java
@@ -0,0 +1,21 @@
+package com.example.examplemod;
+
+import com.example.examplemod.common.ArrowNock;
+
+import net.minecraft.init.Blocks;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+
+@Mod(modid = SeansMod.MODID, version = SeansMod.VERSION)
+public class SeansMod
+{
+    public static final String MODID = "seansmod";
+    public static final String VERSION = "1.0";
+
+    @EventHandler
+    public static void init(FMLInitializationEvent event) {
+      MinecraftForge.EVENT_BUS.register(ArrowNock.class);
+    }
+}
