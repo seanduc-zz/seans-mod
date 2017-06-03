bc2620d4c475b8d74704eaa50ea977997747c064
Dru Jensen <drujensen@gmail.com>
2017-06-03 09:53:33 -0700

    swing when arrow nocked

--- src/main/java/com/example/examplemod/common/ArrowNock.java
+++ src/main/java/com/example/examplemod/common/ArrowNock.java
@@ -1,18 +1,16 @@
 package com.example.examplemod.common;
 
-import net.minecraft.entity.MoverType;
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraftforge.common.MinecraftForge;
+import net.minecraft.util.EnumHand;
 import net.minecraftforge.event.entity.player.ArrowNockEvent;
-import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 public class ArrowNock {
     @SubscribeEvent
     public static void arrowNocked(ArrowNockEvent event) {
         EntityPlayer player = event.getEntityPlayer();
-        
-        MinecraftForge.EVENT_BUS.post(new PlayerInteractEvent.RightClickBlock(player, null, null, null, null));
-    	System.out.println("Arrow nocked! right click block.");
+        player.swingArm(EnumHand.OFF_HAND);
+
+    	System.out.println("Arrow nocked!");
     }
 }
